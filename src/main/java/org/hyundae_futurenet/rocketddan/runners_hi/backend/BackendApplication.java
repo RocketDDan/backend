1818804c01a55837f3f61bf42e2052d959e8d66f
JaeY0ungJung <hanol5216@gmail.com>
2025-06-01 12:37:09 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package org.hyundae_futurenet.rocketddan.runners_hi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package org.hyundae_futurenet.rocketddan.runners_hi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
