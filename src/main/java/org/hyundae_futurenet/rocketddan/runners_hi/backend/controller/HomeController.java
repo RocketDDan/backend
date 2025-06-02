d3739eee1bc74b3cb141a3e7f41ea262ab1cd967
JaeY0ungJung <hanol5216@gmail.com>
2025-06-02 14:49:51 +0900

    add: swagger 추가

--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/controller/HomeController.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/controller/HomeController.java
@@ -1,5 +1,7 @@
 package org.hyundae_futurenet.rocketddan.runners_hi.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.hyundae_futurenet.rocketddan.runners_hi.backend.repository.TestRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Test API", description = "Test: 시간 목록")
 public class HomeController {
 
     private final TestRepository testRepository;
 
+    @Operation(summary = "시간 조회", description = "시간을 조회합니다.")
     @GetMapping("/")
     public String home() {
 
