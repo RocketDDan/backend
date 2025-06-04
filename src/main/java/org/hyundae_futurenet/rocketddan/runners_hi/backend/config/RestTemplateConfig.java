91a9d5d20b921ac8ec074d152d7d10f9f6474e53
JaeY0ungJung <hanol5216@gmail.com>
2025-06-04 14:52:33 +0900

    add: restTemplate 설정 생성

---
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/config/RestTemplateConfig.java
@@ -0,0 +1,15 @@
+package org.hyundae_futurenet.rocketddan.runners_hi.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+	@Bean
+	public RestTemplate restTemplate() {
+
+		return new RestTemplate();
+	}
+}
