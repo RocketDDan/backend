42be325f32776b326fcf520fbb27b9208ef9439d
JaeY0ungJung <hanol5216@gmail.com>
2025-06-04 16:41:30 +0900

    style: 코드 컨벤션에 맞게 자동 수정

--- src/test/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/util/file/CloudFrontFileUtilTest.java
+++ src/test/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/util/file/CloudFrontFileUtilTest.java
@@ -1,35 +1,34 @@
 package org.hyundae_futurenet.rocketddan.runners_hi.backend.util.file;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.hyundae_futurenet.rocketddan.runners_hi.backend.config.CloudFrontProperties;
 import org.junit.jupiter.api.Test;
 
 class CloudFrontFileUtilTest {
 
-    @Test
-    void generateSignedUrl() throws Exception {
-        // given
-        CloudFrontProperties props = new CloudFrontProperties();
-        props.setDistributionDomain("d3j62hm9xnqci3.cloudfront.net");
-        props.setKeyPairId("K1CXY699RY3FDX");
-        props.setPrivateKeyPath(
-            "/Users/jeongjaeyeong/secrets/runners-hi/aws-cloudfront/private_key.pem");
+	@Test
+	void generateSignedUrl() throws Exception {
+		// given
+		CloudFrontProperties props = new CloudFrontProperties();
+		props.setDistributionDomain("d3j62hm9xnqci3.cloudfront.net");
+		props.setKeyPairId("K1CXY699RY3FDX");
+		props.setPrivateKeyPath(
+			"/Users/jeongjaeyeong/secrets/runners-hi/aws-cloudfront/private_key.pem");
 
-        CloudFrontFileUtil util = new CloudFrontFileUtil(props);
+		CloudFrontFileUtil util = new CloudFrontFileUtil(props);
 
-        String objectKey = "test/metamong.jpeg";
-        long validSeconds = 60 * 10; // 10분
+		String objectKey = "test/metamong.jpeg";
+		long validSeconds = 60 * 10; // 10분
 
-        // when
-        String signedUrl = util.generateSignedUrl(objectKey, validSeconds);
+		// when
+		String signedUrl = util.generateSignedUrl(objectKey, validSeconds);
 
-        // then
-        System.out.println("Signed URL: " + signedUrl);
-        assertNotNull(signedUrl);
-        assertTrue(signedUrl.contains("Expires"));
-        assertTrue(signedUrl.contains("Signature"));
-        assertTrue(signedUrl.contains("Key-Pair-Id"));
-    }
+		// then
+		System.out.println("Signed URL: " + signedUrl);
+		assertNotNull(signedUrl);
+		assertTrue(signedUrl.contains("Expires"));
+		assertTrue(signedUrl.contains("Signature"));
+		assertTrue(signedUrl.contains("Key-Pair-Id"));
+	}
 }
