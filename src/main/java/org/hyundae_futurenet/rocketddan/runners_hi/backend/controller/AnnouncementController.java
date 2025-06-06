2304b317325a9f9c063398071ecf5fe67b45cc43
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-06-06 22:01:14 +0900

    feat: 공지사항 등록 API 유효성 검사를 위한 어노테이션 설정

--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/controller/AnnouncementController.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/controller/AnnouncementController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,7 +19,7 @@ public class AnnouncementController {
 	private final AnnouncementFacade announcementFacade;
 
 	@PostMapping
-	public ResponseEntity<Void> createAnnouncement(@RequestBody AnnouncementCreateRequest request) {
+	public ResponseEntity<Void> createAnnouncement(@Valid @RequestBody AnnouncementCreateRequest request) {
 		// 임시 member_id와 role을 1과 USER로 지정 - JWT 토큰에서 member_id와 role를 받아올 예정
 		Long memberId = 6L;
 		// 테스트
--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/model/dto/request/AnnouncementCreateRequest.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/model/dto/request/AnnouncementCreateRequest.java
@@ -1,6 +1,8 @@
 package org.hyundae_futurenet.rocketddan.runners_hi.backend.model.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,11 +18,16 @@ import lombok.ToString;
 public class AnnouncementCreateRequest {
 
 	@Schema(description = "공지 제목", example = "크루 관련 공자")
+	@NotBlank(message = "제목을 입력해주세요.")
+	@Size(max = 100, message = "제목은 100자 이하로 입력해주세요.")
 	private String title;
 
 	@Schema(description = "공지 내용", example = "크루 모집 관련해서 공지합니다.")
+	@NotBlank(message = "내용을 입력해주세요.")
+	@Size(max = 2000, message = "내용은 2000자 이하로 입력해주세요.")
 	private String content;
 
 	@Schema(description = "첨부 파일 경로 (S3)", example = "release/announcement/sample.jpg (임시)")
+	@Size(max = 1000, message = "첨부파일 경로는 1000자 이하로 입력해주세요.")
 	private String attachPath;
 }
