e8dc377e79e14cbce54fea5bd1b6296e1d81c332
JaeY0ungJung <hanol5216@gmail.com>
2025-06-08 01:38:52 +0900

    add: 피드 좋아요 및 취소 컨트롤러 swagger에 설명 추가

--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/controller/FeedLikeController.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/controller/FeedLikeController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Feed Like API", description = "Feed Like")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/feeds/{feed-id}/likes")
@@ -17,19 +20,21 @@ public class FeedLikeController {
 
 	private final FeedFacade feedFacade;
 
+	@Operation(summary = "Feed 좋아요", description = "Feed에 좋아요를 추가합니다.")
 	@PostMapping
 	public ResponseEntity<Void> addLike(@PathVariable("feed-id") long feedId) {
 
 		long loginMemberId = 1L;
-		feedFacade.like(loginMemberId, feedId);
+		feedFacade.likeFeed(loginMemberId, feedId);
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "Feed 좋아요 취소", description = "Feed에 달린 좋아요를 취소합니다.")
 	@DeleteMapping
 	public ResponseEntity<Void> removeLike(@PathVariable("feed-id") long feedId) {
 
 		long loginMemberId = 1L;
-		feedFacade.unlike(loginMemberId, feedId);
+		feedFacade.unlikeFeed(loginMemberId, feedId);
 		return ResponseEntity.ok().build();
 	}
 }
