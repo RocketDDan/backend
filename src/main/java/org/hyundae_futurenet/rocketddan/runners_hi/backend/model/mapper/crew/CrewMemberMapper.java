a6ee4b9f473be2822589964a34e75277ae76d31c
HideOnCodec <tlsdmsgp33@naver.com>
2025-06-08 20:42:52 +0900

    feat: Crew mapper 추가

--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/model/mapper/crew/CrewMemberMapper.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/model/mapper/crew/CrewMemberMapper.java
@@ -1,17 +1,22 @@
 package org.hyundae_futurenet.rocketddan.runners_hi.backend.model.mapper.crew;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Param;
+import org.hyundae_futurenet.rocketddan.runners_hi.backend.model.dto.response.crew.CrewMemberDetailResponse;
 
 @Mapper
 public interface CrewMemberMapper {
 
-	// TODO : 임시 메서드 (삭제하고 사용하세요.)
-	@Select("""
-		    SELECT *
-		    FROM CREW_MEMBER
-		""")
-	List<Object> findAll();
+	// 크루장 여부 조회
+	boolean isLeaderByMemberIdAndCrewId(@Param("crewId") long crewId, @Param("memberId") long memberId);
+
+	// 크루 멤버 여부 조회
+	boolean isCrewMemberByMemberIdAndCrewId(@Param("crewId") long crewId, @Param("memberId") long memberId);
+
+	// 크루 멤버 정보 조회
+	Optional<CrewMemberDetailResponse> selectCrewMemberByCrewIdAndMemberId(
+		@Param("crewId") long crewId,
+		@Param("memberId") long memberId);
 }
