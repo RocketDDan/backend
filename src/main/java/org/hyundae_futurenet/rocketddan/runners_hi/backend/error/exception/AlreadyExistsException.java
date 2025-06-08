078cb74adfb63c72bc5d79c8eaf77fea7a1c4b9f
HideOnCodec <tlsdmsgp33@naver.com>
2025-06-08 20:45:10 +0900

    feat: CustomException 추가

--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/error/exception/AlreadyExistsException.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/error/exception/AlreadyExistsException.java
@@ -1,2 +1,10 @@
-package org.hyundae_futurenet.rocketddan.runners_hi.backend.error.exception;public class AlreadyExistsException {
+package org.hyundae_futurenet.rocketddan.runners_hi.backend.error.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+
+	public AlreadyExistsException(String message) {
+
+		super(message);
+	}
+
 }
--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/error/exception/NotFoundException.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/error/exception/NotFoundException.java
@@ -1,2 +1,9 @@
-package org.hyundae_futurenet.rocketddan.runners_hi.backend.error.exception;public class NotFoundException {
+package org.hyundae_futurenet.rocketddan.runners_hi.backend.error.exception;
+
+public class NotFoundException extends RuntimeException {
+
+	public NotFoundException(String message) {
+
+		super(message);
+	}
 }
