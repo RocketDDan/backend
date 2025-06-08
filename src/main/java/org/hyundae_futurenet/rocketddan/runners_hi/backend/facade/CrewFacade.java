1245d1b46956391b3e530f18c248aa17009838d9
HideOnCodec <tlsdmsgp33@naver.com>
2025-06-08 20:45:34 +0900

    feat: Crew 기본 crud 서비스 구현

--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/facade/CrewFacade.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/facade/CrewFacade.java
@@ -1,5 +1,28 @@
 package org.hyundae_futurenet.rocketddan.runners_hi.backend.facade;
 
+import java.util.List;
+
+import org.hyundae_futurenet.rocketddan.runners_hi.backend.model.dto.request.crew.CrewCreateRequest;
+import org.hyundae_futurenet.rocketddan.runners_hi.backend.model.dto.request.crew.CrewSearchFilter;
+import org.hyundae_futurenet.rocketddan.runners_hi.backend.model.dto.request.crew.CrewUpdateRequest;
+import org.hyundae_futurenet.rocketddan.runners_hi.backend.model.dto.response.crew.CrewDetailResponse;
+import org.hyundae_futurenet.rocketddan.runners_hi.backend.model.dto.response.crew.CrewListResponse;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface CrewFacade {
 
+	// 크루 생성
+	long insertCrew(long loginMemberId, CrewCreateRequest crewCreateRequest, MultipartFile file);
+
+	// 크루 수정
+	void updateCrew(long loginMemberId, long crewId, CrewUpdateRequest crewUpdateRequest, MultipartFile file);
+
+	// 크루 삭제
+	void deleteCrew(long loginMemberId, long crewId);
+
+	// 크루 프로필 조회
+	CrewDetailResponse selectCrewByCrewId(long loginMemberId, long crewId);
+
+	// 크루 목록 조회
+	List<CrewListResponse> selectCrewsByFilter(long loginMemberId, CrewSearchFilter crewSearchFilter);
 }
