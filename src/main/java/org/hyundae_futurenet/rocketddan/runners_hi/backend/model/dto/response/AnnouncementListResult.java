a4ca3fee6502525724c8659de0c7a6b2941c1d2d
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-06-17 11:34:43 +0900

    feat: 공지관련 dto에 swagger 작성

--- src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/model/dto/response/AnnouncementListResult.java
+++ src/main/java/org/hyundae_futurenet/rocketddan/runners_hi/backend/model/dto/response/AnnouncementListResult.java
@@ -2,6 +2,7 @@ package org.hyundae_futurenet.rocketddan.runners_hi.backend.model.dto.response;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AnnouncementListResult {
 
+	@Schema(description = "공지 목록")
 	private List<AnnouncementListResponse> announcements;
 
+	@Schema(description = "전체 공지 개수", example = "27")
 	private int totalCount;
 }
